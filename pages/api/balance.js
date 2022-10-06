5856dfe765aab8b4f8bab160888f394378e478ab
auswitch <sawit_cha@cmu.ac.th>
2022-10-06 15:24:15 +0700

    completed

--- pages/api/balance.js
+++ pages/api/balance.js
@@ -5,9 +5,16 @@ export default function balanceRoute(req, res) {
   if (req.method === "GET") {
     //check authentication
     const user = checkToken(req);
-    // return res.status(403).json({ok: false,message: "You do not have permission to check balance",});
+    if(!user || user.isAdmin)
+    {
+      return res.status(403).json({ok: false,message: "You do not have permission to check balance",});
+    }
 
     const users = readUsersDB();
+    const foundUser = users.find(
+      (x) => x.username === user.username);
+
+    return res.json({ ok:true, money: foundUser.money });
     //find user in DB and get their money value
 
     //return response
--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,10 +1,32 @@
+import { readUsersDB } from "../../backendLibs/dbLib";
+import { checkToken } from "../../backendLibs/checkToken";
+
 export default function summaryRoute(req, res) {
-  if (req.method === "GET") {
+  if (req.method === "GET") 
+  {
     //check authentication
-    //return res.status(403).json({ ok: false, message: "Permission denied" });
+    const user = checkToken(req);
+    if(!user || !user.isAdmin)
+    {
+      return res.status(403).json({ ok: false, message: "Permission denied" });
+    }
+
     //compute DB summary
+    const users = readUsersDB();
+    const userCount = users.filter((x) => x.isAdmin === false);
+    const adminCount = users.filter((x) => x.isAdmin === true);
+    const totalMoney = [0, 0];
+    for(totalMoney[1] = 0; totalMoney[1] < userCount.length; totalMoney[1]++)
+    {
+      totalMoney[0] += userCount[totalMoney[1]].money;
+    }
+    
+
+    return res.json({ ok: true, userCount: userCount.length, 
+                      adminCount: adminCount.length, totalMoney: totalMoney[0] });
     //return response
-  } else {
+  } 
+  else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
 }
